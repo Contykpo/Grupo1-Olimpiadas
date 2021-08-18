24b15e332c7ec38d98a10a5e7dae9bbaa1cc61bf
Crazy474747 <58375872+Crazy474747@users.noreply.github.com>
2021-08-18 17:10:25 -0300

    Ver descripcion
    Creada estructura inicial del proyecto

---
+++ Olimpiadas.AdministradorGeneral/Olimpiadas.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Olimpiadas.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ Olimpiadas.AdministradorGeneral/Olimpiadas.RecursosWPF/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Olimpiadas.RecursosWPF
+{
+	public class Class1
+	{
+	}
+}
